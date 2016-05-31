241c9d12e758a076407c613addaddcb8784c034c
Keira Rochester <keira@keira.local>
2016-05-31 14:36:43 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#1. Calculates a 15% tip on a $55 meal
+tip = 55 * 0.15
+
+puts "A 15% tip on a $55 bill would be #{tip}"
+
+#2. Adding string and integer with + operator
+puts (400).to_s + " Poops"
+
+multiply = 45628 * 7839
+
+#3. multiply 45628 * 7839 in a sentence
+puts "45628 * 7839 = #{multiply}"
+
+#4. The value of this expression is True
+puts (10 < 20 && 30 < 20) || !(10 == 11)
