070a55e66df7c30e461dfed4acca50917a25f305
Keira Rochester <keira@keira.local>
2016-05-31 15:24:02 -0400

    Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
 puts "What's your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+time = Time.now
+
+birth_year = time.year - age.to_i
+
+puts "Hey #{name}!! You were born in year #{birth_year}!"
